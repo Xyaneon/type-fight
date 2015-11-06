257b2a8cf6891f426212a7660256ad4d1759e6f6
Christopher Kyle Horton <christhehorton@gmail.com>
2015-11-06 16:22:27 -0500

    Start implementing state transitions for the Opponent.

--- opponent.py
+++ opponent.py
@@ -15,6 +15,7 @@ class Opponent:
         self.health_percent = 100
         self.attack_delay_frames = 0
         self.state = 'idle'
+        self.state_frames_remaining = 120
         self.weak_spots = []
 
     def get_rect(self):
@@ -24,10 +25,31 @@ class Opponent:
     def take_damage(self, damage):
         '''Deals damage to this Opponent.'''
         self.health_percent -= damage
+        self.state_transition('damaged', 0.5)
         if self.health_percent <= 0:
             self.health_percent = 0
             self.state = 'defeated'
             self.updown_juice = 0
+
+    def state_transition(self, state, state_seconds):
+        '''Go into the next state for so many seconds.'''
+        self.state = state
+        self.state_frames_remaining = int(state_seconds * 60)
+
+    def update_state(self):
+        '''Implements a simple state machine for determing the Opponent's
+        next state and how much more time remains in the current one.'''
+        if self.state not in ['defeated']:
+            self.state_frames_remaining -= 1
+            if self.state_frames_remaining <= 0:
+                if self.state == 'idle':
+                    # Start charging an attack
+                    self.state_transition('charging', 3)
+                elif self.state == 'damaged':
+                    self.state_transition('idle', 2)
+                if self.state == 'charging':
+                    # TODO: implement an attack here
+                    self.state_transition('idle', 2)
 
     def render(self):
         '''Returns a pygame.Surface with the rendered opponent.'''
@@ -35,7 +57,7 @@ class Opponent:
         self.surface.fill(pygame.color.Color(0, 0, 0, 0))
         center_rect = self.rect.copy()
         center_rect.centerx = self.surface.get_rect().centerx
-        if self.state == 'idle':
+        if self.state in ['idle', 'damaged', 'charging']:
             self.updown_juice += (2.0 * math.pi) / 60.0
             center_rect.centery = self.surface.get_rect().centery + 10 * math.sin(self.updown_juice)
         elif self.state == 'defeated':
