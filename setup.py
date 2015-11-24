f93c1419444a7260b3adfdcab2e420e60712fbb6
Christopher Kyle Horton <christhehorton@gmail.com>
2015-11-24 15:41:31 -0500

    Update for version 0.3 MSI generation.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ executables = [
 ]
 
 setup(name='TypeFight',
-      version = '0.2',
+      version = '0.3',
       description = 'A typing game with fighting robots',
       options = dict(build_exe = buildOptions, bdist_msi = bdist_msi_options),
       executables = executables)
