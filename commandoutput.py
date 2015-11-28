0754f4bb576d10ea223b9f7b382366a69023f60c
Christopher Kyle Horton <christhehorton@gmail.com>
2015-11-28 17:15:23 -0500

    Add start of module for an output window in the HUD for player feedback.

---
+++ commandoutput.py
@@ -0,0 +1,35 @@
+#!/bin/python
+
+import pygame
+
+pygame.font.init()
+output_font = pygame.font.Font("fonts/Share_Tech_Mono/ShareTechMono-Regular.ttf", 16)
+output_bkg_color = pygame.color.Color(0, 0, 0, 0.5)
+output_text_color = pygame.color.Color(0, 255, 0, 0.75)
+
+output_height = 300
+output_width = 350
+max_output_lines = 5
+
+class CommandOutput():
+    '''Class for displaying flavor text feedback to the player in a chat box
+    or log format.'''
+    def __init__(self):
+        self.surface = pygame.Surface((output_width, output_height), pygame.SRCALPHA)
+        self.rect = self.surface.get_rect()
+        self.text_lines = []
+        self.text_surface = command_font.render('>', True, command_text_color)
+
+    def render(self):
+        '''Returns a pygame.Surface containing the rendered output window and
+        text.'''
+        padding = 10
+        self.surface.fill(pygame.Color(0, 0, 0, 0))
+
+        border_rect = self.surface.get_rect()
+        pygame.draw.rect(self.surface, output_bkg_color, border_rect)
+        if self.text_surface is not None:
+            text_left_align = padding
+            text_top_align = padding
+            self.surface.blit(self.text_surface, (text_left_align, text_top_align))
+        return self.surface
