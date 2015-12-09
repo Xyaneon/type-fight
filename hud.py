ebd8a905edcb09a06e651587f8bd0f3173c01739
Christopher Kyle Horton <christhehorton@gmail.com>
2015-12-08 23:50:47 -0500

    Fix #21 and correctly center the command prompt.

--- hud.py
+++ hud.py
@@ -1,6 +1,8 @@
 #!/bin/python
 
 import os, pygame
+from commandentry import prompt_width
+from gameglobals import *
 
 opponent_name_font = pygame.font.Font("fonts/Share_Tech_Mono/ShareTechMono-Regular.ttf", 28)
 
@@ -43,7 +45,7 @@ class Hud:
         # Main HUD portion
         self.surface.fill(pygame.color.Color(0, 0, 0, 0))
         self.surface.blit(self.hud_bkg_image, self.hud_bkg_image.get_rect())
-        self.surface.blit(c_entry.surface, (315, 775))
+        self.surface.blit(c_entry.surface, (DEFAULT_WINDOW_CENTERX - prompt_width / 2, 845))
         self.surface.blit(c_output.surface, (34, 90))
         # Health bars
         phfr = self.player_health_fg_rect.copy()
